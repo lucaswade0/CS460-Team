82d6ed019c350bcc7c6b0041db4a167b4ffee3af
lucaswade0 <lucaswade1313@gmail.com>
2025-11-06 13:17:29 -0800

    Adding ASTBuilder Class

---
+++ ASTBuilder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Lucas Wade on 11/6/25.
+//
+
+#include "ASTBuilder.h"
---
+++ ASTBuilder.h
@@ -0,0 +1,13 @@
+//
+// Created by Lucas Wade on 11/6/25.
+//
+
+#ifndef UNTITLED1_ASTBUILDER_H
+#define UNTITLED1_ASTBUILDER_H
+
+
+class ASTBuilder {
+};
+
+
+#endif //UNTITLED1_ASTBUILDER_H
